3d0a26895f7a1855fdbf3c51ef8d9de9b963d47c
lilhamma <lilhamma@ya.ru>
2017-02-22 13:59:43 +0500

    Fixed the element category table Had integer name and not-null parent

--- migrations/m161115_143012_store_element_category.php
+++ migrations/m161115_143012_store_element_category.php
@@ -22,8 +22,8 @@ class m161115_143012_store_element_category extends Migration
             'store_element_category',
             [
                 'id'=> $this->primaryKey(11),
-                'name'=> $this->integer(11)->notNull(),
-                'parent_id'=> $this->integer(11)->notNull(),
+                'name'=> $this->string(255)->notNull(),
+                'parent_id'=> $this->integer(11),
             ],$tableOptions
         );
         $this->createIndex('fk_store_element_category_store_element_category1_idx','{{%store_element_category}}','parent_id',false);
